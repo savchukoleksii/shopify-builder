317d769fd1e1055e6c28a459687d905d322c77ba
Oleksii Savchuk <savchukoleksii@gmail.com>
2023-01-16 22:29:29 +0200

    Fix replace function

--- builder/helpers/entries.js
+++ builder/helpers/entries.js
@@ -3,7 +3,7 @@ const glob			= require('glob');
 const path  		= require("path");
 
 module.exports = (files) => {
-	const filepath = path.join(themeRoot, files).replaceAll("\\", "/");
+	const filepath = path.join(themeRoot, files).replace(/\\/g, "/");
 
 	return glob.sync(filepath).reduce(function(entries, entry){
 		const parsed_entry = path.parse(entry);
@@ -11,7 +11,7 @@ module.exports = (files) => {
 
 		return {
 			...entries,
-			[`${directory !== "scripts" ? `${directory}-` : ''}${parsed_entry.name.replaceAll(".", '-')}`]: entry
+			[`${directory !== "scripts" ? `${directory}-` : ''}${parsed_entry.name.replace("/\./g", '-')}`]: entry
 		};
 	}, {});
 };
